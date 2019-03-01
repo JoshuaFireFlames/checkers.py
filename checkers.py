50618ead33c5bf38c07430631e1d7cacaaac8d69
csPython <cspython@AD25TF04NGF1J-59.local>
2019-03-01 09:32:50 -0500

    (Empty Square)

--- checkers.py
+++ checkers.py
@@ -3,7 +3,10 @@ from graphics import*
 sqSz = 50
 
 chWin = GraphWin("Checkers", sqSz *10 ,sqSz *10)
-chWin.setcoords(0, 0, sqSz*10 , sqSz *10)
+chWin.setCoords(0, 0, sqSz * 10 , sqSz *10)
+
+sQ = Rectangle(Point (sqSz, sqSz), (Point (sqSz*2,sqSz *2)))
+sQ.draw(chWin)
 
 chWin.getmouse()
 chWin.close()
