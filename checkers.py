b71421c85b2009ca69598d41c7c60979bb3ad33f
csPython <cspython@lpf0lwntb.mbhs.lab>
2019-03-12 11:51:31 -0400

    The user can set a color for the grid.

--- checkers.py
+++ checkers.py
@@ -8,18 +8,22 @@ def draw_sq(sX, sY,size, color , win):
     square.draw(win)
 
 sqSz = 50
-sqCol = "red"
 
 chWin = GraphWin("Checkers", sqSz *10 ,sqSz *10)
 chWin.setCoords(0, 0, sqSz * 10 , sqSz *10)
+
+grCol = input("How do you want to color the checker board ?")
+ 
+ 
 
 for j in range (8):
     for i in range (8):
         if (i + j) % 2 == 0:
             sqCol = "black"
         else:
-            sqCol = "red"
+            sqCol = grCol
         draw_sq(sqSz * (i +1) , sqSz * (j + 1), sqSz, sqCol, chWin)
 
-chWin.getmouse()
+
+chWin.getMouse()
 chWin.close()
